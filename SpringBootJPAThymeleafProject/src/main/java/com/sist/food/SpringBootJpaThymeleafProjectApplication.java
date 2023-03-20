2182b861fed4183271f652e4daa6b337a23338d8
user <user@DESKTOP-COJ9858>
2023-03-20 17:49:56 +0900

    Spring thymeleaf

---
+++ SpringBootJPAThymeleafProject/src/main/java/com/sist/food/SpringBootJpaThymeleafProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sist.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootJpaThymeleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaThymeleafProjectApplication.class, args);
+	}
+
+}
